a263a5afcdab5112f24118cd1eff593d7616bbd2
jcrab <jcrabtree@una.edu>
2024-02-05 08:49:53 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cookie Demo</title>
+    </head>
+    <body>
+        <form action="welcome.php" method="post">
+            Name: <input type="text" name="name" />
+            Password: <input type="password" name="pwd" />
+            Remember Me: <input type="checkbox" name="remember" value="ON" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
