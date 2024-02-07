ee60dfc1eaa8ca075e0b74ea56595c88b660b0c9
Crabtree <jcrabtree@una.edu>
2024-02-07 15:49:33 -0600

    fixed

--- index.php
+++ index.php
@@ -5,11 +5,19 @@
         <title>Cookie Demo</title>
     </head>
     <body>
+        <?php
+        if (isset($_COOKIE['user'])) {
+            echo "Welcome back " . $_COOKIE['user'] . "<br>";
+        } else {
+        ?>
         <form action="welcome.php" method="POST">
             Name: <input type="text" name="name" /><br>
             Password: <input type="password" name="pwd" /><br>
             Remember Me: <input type="checkbox" name="remember" value="ON" /><br>
             <input type="submit" />
         </form>
+        <?php
+        }
+        ?>
     </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -1,21 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // if remember checked set cookie
-        $user = $_POST['name'];
-        $pwd = $_POST['pwd'];
-        
-        if ($pwd == "bar") {
-            echo "Welcome $user";
-        } else {
-            echo "bad username or password";
-        }
-        
-        ?>
-    </body>
-</html>
+<?php
+
+// if remember checked set cookie
+$username = $_POST['name'];
+$pwd = $_POST['pwd'];
+
+if ($pwd == "bar") {
+    echo "Welcome $username";
+    if (isset($_POST['remember']) && $_POST['remember'] == "ON") {
+        echo "remember";
+        setcookie("user", $username, time() + (60 * 2));
+    } else {
+        echo "forget";
+    }
+} else {
+    echo "bad username or password";
+}
+
